d3424a9228c67684321b598d1b04168d2a0bd9ff
enso-x <ggsi.anonymous@gmail.com>
2023-01-16 08:36:30 +0300

    Added content caching

--- pages/api/logs.ts
+++ pages/api/logs.ts
@@ -4,6 +4,45 @@ import { NextResponse } from 'next/server';
 
 const ERROR_RESPONSE: string = '';
 
+const cachedData: Record<string, string> = {};
+const runningLoops: Record<string, boolean> = {};
+const timeoutIDs: Record<string, NodeJS.Timeout> = {};
+
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+const updateLoop = (url: string) => {
+	const timeoutKey = `cancel-update-${url}`;
+	if (timeoutIDs[timeoutKey]) {
+		clearTimeout(timeoutIDs[timeoutKey]);
+		delete timeoutIDs[timeoutKey];
+	}
+	timeoutIDs[timeoutKey] = setTimeout(() => {
+		runningLoops[url] = false;
+		delete runningLoops[url];
+		delete cachedData[url];
+		delete timeoutIDs[timeoutKey];
+	}, 30 * 1000);
+};
+
+const initFetchDataLoop = (url: string) => {
+	runningLoops[url] = true;
+
+	const update = async (): Promise<string> => {
+		if (runningLoops[url]) {
+			const response = await fetch(url);
+			cachedData[url] = await response.text();
+
+			await delay(1000);
+			update();
+		}
+		return cachedData[url];
+	};
+
+	updateLoop(url);
+
+	return update;
+};
+
 const handler = async (
 	req: NextApiRequest,
 	res: NextApiResponse<string>
@@ -12,10 +51,16 @@ const handler = async (
 		res.status(404).write(ERROR_RESPONSE);
 	}
 
-	const response = await fetch(req.query.url as string);
-	const text = await response.text();
+	const queryUrl = req.query.url as string;
 
-	res.status(200).send(text);
+	if (!cachedData[queryUrl]) {
+		const getData = initFetchDataLoop(queryUrl);
+		const text = await getData();
+		res.status(200).send(text);
+	} else {
+		updateLoop(queryUrl);
+		res.status(200).send(cachedData[queryUrl]);
+	}
 };
 
 export default handler;
